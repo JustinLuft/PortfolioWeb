cb7be79e982ef90755ad84fc9bf826adff82b62a
JustinLuft <89544777+JustinLuft@users.noreply.github.com>
2025-05-27 13:40:40 -0400

    Update App.tsx

--- Root Directory/src:/App.tsx
+++ Root Directory/src:/App.tsx
@@ -37,24 +37,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-```
-
-This `App.tsx` is very similar to the `main.tsx`, with a few key differences:
-
-1. It's a separate component that can be imported into `main.tsx`
-2. Uses explicit `React.FC` type for the component
-3. Exports the component as default
-
-To use this, you would modify `main.tsx` to import and use this `App` component:
-
-```tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
